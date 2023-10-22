80287844e8b0e3ea9877321cc18ca35c365cb5cd
abhinandpn <abhinandpn4067@gmail.com>
2023-10-23 01:00:13 +0530

    fialinz

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	var ar = []int{2, 3, 4, 2, 2, 3, 7, 3, 8, 3,2}
+	// Solution for 169
+	var ar = []int{2, 3, 4, 2, 2, 3, 7, 3, 8, 3, 2}
 	res := Golang.MajorityElement(ar)
 	fmt.Println(res)
 }
